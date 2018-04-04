d2523fd75640abd0cb879ed868e62cec7ef45815
Kostya K <f.o.u.n.d.0@gmail.com>
2018-04-04 17:33:34 +0300

    Travis: disable Windows build.

--- travis-scripts/install.linux.sh
+++ travis-scripts/install.linux.sh
@@ -10,11 +10,11 @@ pyinstaller -D -F -n telegram-chat-dump -c ./telegram_messages_dump/run.py
 
 
 #Setup build env for Windows: pyinstaller+telethon
-wine pip install -I telethon==0.17
-wine pip install pyinstaller
-wine pyinstaller -n telegram-chat-dump.exe --onefile --clean --win-private-assemblies -c --noconfirm --log-level=WARN ./telegram_messages_dump/run.py
+# wine pip install -I telethon==0.17
+# wine pip install pyinstaller
+# wine pyinstaller -n telegram-chat-dump.exe --onefile --clean --win-private-assemblies -c --noconfirm --log-level=WARN ./telegram_messages_dump/run.py
 
 #Package for distribution
 cd ./dist
 tar -zcvf telegram_chat_dump_linux.tar.gz ./telegram-chat-dump
-zip -r telegram_chat_dump_windows.zip ./telegram-chat-dump.exe
+# zip -r telegram_chat_dump_windows.zip ./telegram-chat-dump.exe
--- travis-scripts/pre_install.linux.sh
+++ travis-scripts/pre_install.linux.sh
@@ -16,14 +16,14 @@ pip install https://github.com/pyinstaller/pyinstaller/archive/develop.zip
 #Build for Windows
 
 #Install wine from their PPA
-sudo dpkg --add-architecture i386
-sudo apt-add-repository 'https://dl.winehq.org/wine-builds/ubuntu/'
-wget https://dl.winehq.org/wine-builds/Release.key && sudo apt-key add Release.key
-sudo apt update && sudo apt install winehq-stable
+# sudo dpkg --add-architecture i386
+# sudo apt-add-repository 'https://dl.winehq.org/wine-builds/ubuntu/'
+# wget https://dl.winehq.org/wine-builds/Release.key && sudo apt-key add Release.key
+# sudo apt update && sudo apt install winehq-stable
 
 #Install win32-python+pip within wine
-wget "https://www.python.org/ftp/python/3.4.4/python-3.4.4.msi"
-wine msiexec /i python-3.4.4.msi /quiet
+# wget "https://www.python.org/ftp/python/3.4.4/python-3.4.4.msi"
+# wine msiexec /i python-3.4.4.msi /quiet
 
 #Install zip for windows deployment
-sudo apt-get install zip
+# sudo apt-get install zip
