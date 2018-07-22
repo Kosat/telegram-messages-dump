7cbfaa0a366e0918749e38e3896d69e1c28c011a
Kostya K <f.o.u.n.d.0@gmail.com>
2018-07-22 15:10:18 +0300

    Travis fix for macOs build.

--- travis-scripts/pre_install.macos.sh
+++ travis-scripts/pre_install.macos.sh
@@ -3,10 +3,18 @@
 brew update
 
 #Use this specific python 3.5.3 version because of 'pyinstaller' compatibility issues
-brew install https://raw.githubusercontent.com/Homebrew/homebrew-core/ec545d45d4512ace3570782283df4ecda6bb0044/Formula/python3.rb
+#brew install https://raw.githubusercontent.com/Homebrew/homebrew-core/ec545d45d4512ace3570782283df4ecda6bb0044/Formula/python3.rb
+
+#brew install python3
+brew upgrade python
 
 #virtualenv env -p python3
 #source env/bin/activate
+echo Which python:
+which python3
+
+#echo Which python3:
+#which python3
 
 echo Current active python version:
 python3 --version
